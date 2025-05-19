fbbd4f18b69ebfefdf24e57efcbc7f3c50ca2533
KenEspera <KenCarlKate@LAPTOP-0QG83DN0>
2025-05-19 09:33:39 +0800

    1.5

--- FinalProject/ExpenseUI.cpp
+++ FinalProject/ExpenseUI.cpp
@@ -2,17 +2,19 @@
 #include "Color.h"
 #include <iostream>
 #include <string>
+#include <algorithm> 
 
 ExpenseUI::ExpenseUI(ExpenseManager& manager, IExpenseStorage& storage)
     : manager(manager), storage(storage) {}
 
 void ExpenseUI::showMenu() {
     int choice;
+    std::string input;
 
     while (true) {
         bool stop = false;
 
-        std::cout << COLOR_CYAN << "\n===== Expense Tracker =====\n" << COLOR_RESET;
+        std::cout << COLOR_CYAN << "\n====== Expense Tracker ======\n" << COLOR_RESET;
         std::cout << COLOR_GREEN << "1." << COLOR_YELLOW << " Add Expense\n";
         std::cout << COLOR_GREEN << "2." << COLOR_YELLOW << " List All Expenses\n";
         std::cout << COLOR_GREEN << "3." << COLOR_YELLOW << " List Expenses by Category\n";
@@ -20,15 +22,17 @@ void ExpenseUI::showMenu() {
         std::cout << COLOR_GREEN << "5." << COLOR_YELLOW << " Show Total Expenses\n";
         std::cout << COLOR_GREEN << "6." << COLOR_YELLOW << " Save Expenses to File\n";
         std::cout << COLOR_GREEN << "7." << COLOR_YELLOW << " Load Expenses from File\n";
-        std::cout << COLOR_GREEN << "0." << COLOR_YELLOW << " Exit\n" << COLOR_RESET;
+        std::cout << COLOR_GREEN << "0." << COLOR_YELLOW << " Exit" << COLOR_RESET;
+        std::cout << COLOR_CYAN << "\n=============================\n" << COLOR_RESET;
         std::cout << COLOR_BOLD << "Select an option: " << COLOR_RESET;
-        std::cin >> choice;
-        if (std::cin.fail()) {
-            std::cout << COLOR_RED << "Invalid Input" << COLOR_RESET << '\n';
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        std::getline(std::cin, input);
+
+        if (input.empty() || !std::all_of(input.begin(), input.end(), ::isdigit)) {
+            std::cout << COLOR_RED << "Invalid input\n" << COLOR_RESET;
             continue;
         }
+        choice = std::stoi(input);
 
         switch (choice) {
         case 1: addExpense(); break;
@@ -50,7 +54,6 @@ void ExpenseUI::addExpense() {
     std::string category, date;
     double amount;
 
-    std::cin.ignore();
     std::cout << "Enter category (b to back): ";
     std::getline(std::cin, category);
     if (category == "b" || category == "B") {
@@ -84,34 +87,40 @@ void ExpenseUI::addExpense() {
 
 void ExpenseUI::listExpenses() {
     const auto& expenses = manager.getAllExpenses();
-    std::cout << COLOR_BOLD << "All Expenses:\n" << COLOR_RESET;
+    std::cout << COLOR_BOLD << "\nAll Expenses:" << COLOR_RESET;
+
+    std::cout << COLOR_CYAN << "\n========== Expense ==========\n" << COLOR_RESET;
     for (const auto& e : expenses) {
         std::cout << COLOR_YELLOW << e.getDate() << COLOR_RESET
             << " | " << COLOR_BLUE << e.getCategory() << COLOR_RESET
             << " | " << COLOR_GREEN << "$" << e.getAmount() << COLOR_RESET << '\n';
     }
+    std::cout << COLOR_CYAN << "=============================\n" << COLOR_RESET;
 }
 
 void ExpenseUI::listByCategory() {
     std::string category;
 
     std::cin.ignore();
-    std::cout << "Enter category (b to back): ";
+    std::cout << "Search for category (b to back): ";
     std::getline(std::cin, category);
     if (category == "b" || category == "B") {
         ExpenseUI::showMenu();
     }
 
     auto filtered = manager.getExpensesByCategory(category);
-    std::cout << COLOR_BOLD << "Expenses in category: " << category << "\n" << COLOR_RESET;
+    std::cout << COLOR_BOLD << "\nExpenses in category: " << category << "\n" << COLOR_RESET;
     if (filtered.empty()) {
         std::cout << COLOR_RED << "No category found" << COLOR_RESET << '\n';
     }
+
+    std::cout << COLOR_CYAN << "========== Expense ==========\n" << COLOR_RESET;
     for (const auto& e : filtered) {
             std::cout << COLOR_YELLOW << e.getDate() << COLOR_RESET
                 << " | " << COLOR_BLUE << e.getCategory() << COLOR_RESET
                 << " | " << COLOR_GREEN << "$" << e.getAmount() << COLOR_RESET << '\n';
     }
+    std::cout << COLOR_CYAN << "=============================\n" << COLOR_RESET;
 }
 
 void ExpenseUI::listByDate() {
